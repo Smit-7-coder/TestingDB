61801d717755c259e19f15a0c8053ef07af38e43
Sakariya Smit R <smit7.sakariya@gmail.com>
2026-05-30 07:24:43 +0530

    First commit

---
+++ Config/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestingDB.Models;
+
+namespace TestingDB.Config
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
+    
---
+++ Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace TestingDB.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public String? Name { get; set; }
+
+        public int Age { get; set; }
+
+    }
+}
